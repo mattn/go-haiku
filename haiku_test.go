a3d1f3587676d29d4cb6e53389e1cc3e429584cd
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-04-27 23:16:04 +0900

    add tests

---
+++ haiku_test.go
@@ -0,0 +1,34 @@
+package haiku
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func testMatch(t *testing.T, filename string, rules []int, judge bool) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.HasPrefix(text, "#") {
+			continue
+		}
+		if Match(text, rules) != judge {
+			t.Fatalf("%q for %q must be %v", text, filename, rules)
+		}
+	}
+}
+
+func TestHaiku(t *testing.T) {
+	testMatch(t, "testdata/haiku.good", []int{5, 7, 5}, true)
+	testMatch(t, "testdata/haiku.bad", []int{5, 7, 5}, false)
+	testMatch(t, "testdata/tanka.good", []int{5, 7, 5, 7, 7}, true)
+	testMatch(t, "testdata/tanka.bad", []int{5, 7, 5, 7, 7}, false)
+}
